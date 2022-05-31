11103e98cc16aec87bb939dcfd3e23fa6ffeafb9
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2022-05-31 23:42:32 +0530

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,153 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="">
+    Current Edit Mode: <strong>{{ editMode }}</strong>
+    <DataTable
+      :value="tableContents"
+      :editMode="editMode"
+      @cell-edit-complete="onCellEditComplete"
+      class="editable-cells-table"
+      responsiveLayout="scroll"
+    >
+      <Column
+        v-for="col of columns"
+        :field="col.field"
+        :header="col.header"
+        :key="col.field"
+        style="width: 25%"
+      >
+        <template #body="{ data, field }">
+          <slot :name="col.field">
+            <small>{{ data[field] }}</small>
+          </slot>
+        </template>
+
+        <template #editor="slotProps">
+          <slot :name="`${col.field}edit`">
+            <InputText
+              v-model="slotProps.data[slotProps.column.field]"
+              autofocus
+            />
+            <button @click="OnEditComplete(slotProps)">EXIT</button>
+          </slot>
+        </template>
+      </Column>
+    </DataTable>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+
+<script>
+// import RhTable from "@/components/RhTable/RhTable.vue";
+// import RhCard from "./components/RhCard/RhCard.vue";
+// import RhChip from "./components/RhChip/RhChip.vue";
+// import RhSelect from "./components/RhSelect/RhSelect.vue";
+// import RhSearchBar from "./components/RhSearchBar/RhSearchBar.vue";
+// import RhTextField from "./components/RhTextField/RhTextField.vue";
+
+import Column from "primevue/column";
+import DataTable from "primevue/datatable";
+import InputText from "primevue/inputtext";
+export default {
+  components: {
+    // RhDashboardLayout,
+    InputText,
+    // RhCard,
+    // RhTable,
+    // RhChip,
+    // RhTextField,
+    Column,
+    // RhSelect,
+    DataTable,
+    // RhSearchBar,
+  },
+  data() {
+    return {
+      value: "1",
+      editMode: "cell",
+      myValue: "1234",
+      tableContents: [
+        {
+          name: "aaaaaaa",
+          project_name: "bbbb",
+          id: "1",
+          default: 11,
+          village: "charwan",
+        },
+        {
+          name: "ccccc",
+          project_name: "ddddd",
+          id: "2",
+          default: 22,
+          village: "oo",
+        },
+        {
+          name: "eeee",
+          project_name: "fffffff",
+          id: "3",
+          default: 33,
+          village: "opppp",
+        },
+      ],
+      isApart: true,
+      columns: [
+        {
+          field: "name",
+          header: "NAME",
+          sortable: true,
+          cellWidth: "30%",
+        },
+        {
+          field: "project_name",
+          header: "PROJECT",
+          sortable: true,
+          cellWidth: "40%",
+        },
+        // {
+        //   field: "status",
+        //   header: "Status",
+        //   type: "select",
+        //   items: ["Available", "Not available", "Sold out"],
+        // },
+
+        {
+          field: "id",
+          header: "IDD",
+          // type: "action",
+          // icon: "pencil",
+          cellWidth: "8%",
+
+          // function: this.openLPBuilder,
+        },
+      ],
+    };
+  },
+  methods: {
+    // auto edit complete
+    onCellEditComplete(ee) {
+      let { data, newValue, field } = ee;
+
+      console.log(data, newValue, field, "KKKKKKK");
+      console.log("ON CELL EDIT COMPLETE", ee.type);
+
+      // ee.stop
+
+      // ee.preventDefault();
+      // data[field] = "AAAAAAAa";
+    },
+
+    // custom edit click ( from text field)
+
+    OnEditComplete(event) {
+      let { data, newValue, field } = event;
+
+      console.log(data, newValue, field);
+
+      console.log("OK CHECK MARK CICKED", event);
+      data[field] = "something new";
+      // this.editMode = "row";
+    },
+  },
+};
+</script>
